0208f9d85cbb3e890c6e6f41ea2233b1ca89b2a2
Weixiong Li <wlii0223-ui@users.noreply.github.com>
2026-05-25 16:28:19 +1000

    Refine atlas layout and chart idioms

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const charts = [
   ["#historical_impact_bubble", "visualisations/13_historical_impact_bubble.vg.json"]
 ];
 
-const ASSET_VERSION = "20260507g";
+const ASSET_VERSION = "20260525b";
 
 function versionedUrl(url) {
   if (/^https?:\/\//.test(url)) {
