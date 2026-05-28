5ca742ec41666bb77339591f305add27c848c4c4
Weixiong Li <wlii0223-ui@users.noreply.github.com>
2026-05-29 03:31:20 +1000

    Add atlas background and size legend

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const charts = [
   ["#historical_impact_bubble", "visualisations/13_historical_impact_bubble.vg.json"]
 ];
 
-const ASSET_VERSION = "20260529c";
+const ASSET_VERSION = "20260529d";
 
 function versionedUrl(url) {
   if (/^https?:\/\//.test(url)) {
