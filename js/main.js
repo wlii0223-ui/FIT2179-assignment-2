60b4a8570be453757b61bb2c59478fec893247a6
Weixiong Li <wlii0223-ui@users.noreply.github.com>
2026-05-29 03:17:35 +1000

    Add focused chart annotations

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const charts = [
   ["#historical_impact_bubble", "visualisations/13_historical_impact_bubble.vg.json"]
 ];
 
-const ASSET_VERSION = "20260529b";
+const ASSET_VERSION = "20260529c";
 
 function versionedUrl(url) {
   if (/^https?:\/\//.test(url)) {
