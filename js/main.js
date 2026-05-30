ac9a72cb2c4e08012480c67e18e819a3fbea7ea9
Weixiong Li <wlii0223-ui@users.noreply.github.com>
2026-05-30 19:21:22 +1000

    Improve informative chart annotations

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const charts = [
   ["#historical_impact_bubble", "visualisations/13_historical_impact_bubble.vg.json"]
 ];
 
-const ASSET_VERSION = "20260530a";
+const ASSET_VERSION = "20260530b";
 
 function versionedUrl(url) {
   if (/^https?:\/\//.test(url)) {
