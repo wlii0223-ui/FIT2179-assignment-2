182ae457430a6e98519b9b16971fc7aeebc6fe39
Weixiong Li <wlii0223-ui@users.noreply.github.com>
2026-05-30 22:55:42 +1000

    Make storytelling copy more natural

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const charts = [
   ["#historical_impact_bubble", "visualisations/13_historical_impact_bubble.vg.json"]
 ];
 
-const ASSET_VERSION = "20260530f";
+const ASSET_VERSION = "20260530g";
 
 function versionedUrl(url) {
   if (/^https?:\/\//.test(url)) {
